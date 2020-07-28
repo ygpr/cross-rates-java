ba202e7aa38d89d951ce96e85c2723dceb6e9573
odysseus <titusdimines@ukr.net>
2020-07-28 17:33:27 +0300

    - fixed test

--- src/test/java/io/github/rates/IntegrationTest.java
+++ src/test/java/io/github/rates/IntegrationTest.java
@@ -1,16 +1,14 @@
 package io.github.rates;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.comparesEqualTo;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.BDDMockito.given;
 
-import io.github.rates.cache.CurrencyRatesCacheUpdater;
 import io.github.rates.cache.CurrencyRatesCache;
+import io.github.rates.cache.CurrencyRatesCacheUpdater;
 import io.github.rates.configurations.CacheUpdateProgram;
 import io.github.rates.domain.Rate;
-import io.github.rates.suppliers.BinanceTargetRatesSupplier;
-import io.github.rates.suppliers.RatesConvertingSupplier;
+import io.github.rates.suppliers.BinanceTargetRatesProvider;
+import io.github.rates.suppliers.CachedRatesSupplier;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -27,21 +25,19 @@ import java.util.concurrent.TimeUnit;
 class IntegrationTest {
 
     @Mock
-    private BinanceTargetRatesSupplier binanceTargetRatesSupplier;
+    private BinanceTargetRatesProvider binanceTargetRatesSupplier;
 
     @Test
     void allFlowTest() throws Exception {
         String asset = "ADA";
         String quotable = "BTC";
-        BigDecimal amount = BigDecimal.valueOf(3.04);
         Rate rate = new Rate(
                 asset, quotable, asset + quotable, 8, 8, BigDecimal.valueOf(4550.9311)
         );
-        BigDecimal expected = amount.multiply(rate.getPrice());
 
         CurrencyRatesCache cache = new CurrencyRatesCache();
         ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
-        RatesConvertingSupplier ratesConvertingSupplier = new RatesConvertingSupplier(cache, executorService);
+        CachedRatesSupplier cachedRatesSupplier = new CachedRatesSupplier(cache, executorService);
         CacheUpdateProgram cacheUpdateProgram = new CacheUpdateProgram(
                 executorService, 0L, 1L, TimeUnit.MINUTES
         );
@@ -52,12 +48,9 @@ class IntegrationTest {
 
         cryptoCurrencyRatesCacheUpdater.startProgram();
 
-        assertEquals(rate, ratesConvertingSupplier.getRateAsynchronously(asset, quotable, 2).get());
-        assertEquals(rate, ratesConvertingSupplier.getRateAsynchronously(asset, quotable).get());
-        assertEquals(rate, ratesConvertingSupplier.getRate(asset, quotable).orElseThrow());
-        assertThat(expected, comparesEqualTo(ratesConvertingSupplier.convertAsynchronously(amount, asset, quotable).get()));
-        assertThat(expected, comparesEqualTo(ratesConvertingSupplier.convertAsynchronously(amount, asset, quotable, 2).get()));
-        assertThat(expected, comparesEqualTo(ratesConvertingSupplier.convert(amount, asset, quotable).orElseThrow()));
+        assertEquals(rate, cachedRatesSupplier.getRateAsynchronously(asset, quotable, 2).get());
+        assertEquals(rate, cachedRatesSupplier.getRateAsynchronously(asset, quotable).get());
+        assertEquals(rate, cachedRatesSupplier.getRate(asset, quotable).orElseThrow());
     }
 
 }
